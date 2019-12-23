6afc9316ca22cccc798ee4e7391a6dbd42d231c8
Ruslan Korkin <rukrak86@gmail.com>
2019-12-23 15:35:15 +0200

    Updates

---
+++ precache-manifest.a7d3725a2ab681410afbfcd3cbf253d1.js
@@ -0,0 +1,31 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c312ccf2a724a206b062be0acda9a5a",
+    "url": "audio/sharp.mp3"
+  },
+  {
+    "revision": "cb60020a6096401ab93d315745c2c2f7",
+    "url": "index.html"
+  },
+  {
+    "url": "js/main.baab53b11b95fa82b8aa.js"
+  },
+  {
+    "url": "js/runtime.baab53b11b95fa82b8aa.js"
+  },
+  {
+    "url": "js/vendors~main.baab53b11b95fa82b8aa.js"
+  },
+  {
+    "revision": "c7d675a6ee5ff4d4681b5ebed7e345de",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "0446b2c711e67fee797934f3d3b1a632",
+    "url": "policy/policy.pdf"
+  },
+  {
+    "revision": "388ed88eec82ddeacbf877ee7dc4b225",
+    "url": "robots.txt"
+  }
+]);
